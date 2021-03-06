3938bc8a05267b0d3da4b084bfc77dd512433d50
Shawn <phi13025@byui.edu>
2021-03-06 12:04:05 -0700

    Type Conversions

--- Week 2/Basics/Basics/Program.cs
+++ Week 2/Basics/Basics/Program.cs
@@ -8,7 +8,7 @@ namespace Basics
         {
             // OverFlow
             byte number = 255;
-            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+
             number += 1;  // Becomes 0
 
             Console.WriteLine("Number: " + number);
@@ -28,7 +28,32 @@ namespace Basics
             Console.WriteLine(isWorking);
             Console.WriteLine(test);
 
+            // Size
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            Console.WriteLine("{0} {1}", int.MinValue, int.MaxValue);
+            Console.WriteLine("{0} {1}", double.MinValue, double.MaxValue);
+            Console.WriteLine("{0} {1}", short.MinValue, short.MaxValue);
 
+            // Constants
+            const float Pi = 3.14f;
+            // Pi = 1; // Does not compile!! Can not change constants 
+            
+            // Type Conversion
+            byte b = 1;
+            int i = b;
+
+            // Won't Compile. It will have data loss
+            // int i = 1;
+            // byte b = i;
+
+            // Cast
+            int num = 1000;
+            byte bit = (byte)num;
+            Console.WriteLine(bit);
+
+            // Use the Convert Class to change data types
+            
 
         }
     }
