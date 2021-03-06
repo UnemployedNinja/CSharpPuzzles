4e9e67a7a33e4e316429eb4012d155043eacf831
Shawn <phi13025@byui.edu>
2021-03-06 11:52:50 -0700

    Data Type and sizes

--- Week 2/Basics/Basics/Program.cs
+++ Week 2/Basics/Basics/Program.cs
@@ -6,11 +6,29 @@ namespace Basics
     {
         static void Main(string[] args)
         {
+            // OverFlow
             byte number = 255;
-
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
             number += 1;  // Becomes 0
 
             Console.WriteLine("Number: " + number);
+
+            // Data Types
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Shawn";
+            bool isWorking = false;
+            var test = false;
+
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+            Console.WriteLine(test);
+
+
 
         }
     }
