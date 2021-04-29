1a39d9b27033365aa254b913e02f442627491176
Shawn <phi13025@byui.edu>
2021-04-28 18:34:55 -0600

    Finished Numbers Game

--- numberGuessingGame/numberGuessingGame/numberGuessingGame/Program.cs
+++ numberGuessingGame/numberGuessingGame/numberGuessingGame/Program.cs
@@ -6,7 +6,40 @@ namespace numberGuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random random = new Random();
+
+            int winNum = random.Next(0, 100);
+
+            bool win = false;
+
+            do
+            {
+                Console.WriteLine("Guess a number in between 0 and 100: ");
+                string s = Console.ReadLine();
+
+                int i = int.Parse(s);
+
+                if (i > winNum)
+                {
+                    Console.WriteLine("To High! Guess Lower...");
+                } 
+                else if (i < winNum)
+                {
+                    Console.WriteLine("To Low! Guess Higher...");
+                }
+                else if (i == winNum)
+                {
+                    Console.WriteLine("YOU WIN!!");
+                    win = true;
+                }
+
+                Console.WriteLine();
+
+            } while (win == false);
+
+            Console.WriteLine("Thank you for playing the game!");
+            Console.WriteLine("Press any key to finished.");
+            Console.ReadKey(true);
         }
     }
 }
