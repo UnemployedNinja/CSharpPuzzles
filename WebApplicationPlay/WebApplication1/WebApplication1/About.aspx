190b7edc8207b02880e651176e5221fb8e310485
Shawn <phi13025@byui.edu>
2021-06-03 11:54:45 -0600

    Added a some new .net

---
+++ WebApplicationPlay/WebApplication1/WebApplication1/About.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="MyFirst.net Play" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="WebApplication1.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <h3>Your application description page.</h3>
+    <p>Use this area to provide additional information.</p>
+
+    <div>
+        <p> I Personally Wrote this part in Abouts.aspx</p>
+    </div>
+</asp:Content>
---
+++ playingWithNetandSQL/TestApp1/TestApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TestApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            SqlConnection connection = new SqlConnection();                 // Connecting to SQL Server
+            SqlCommand command = new SqlCommand();                          // Running a command against the SQL Server instance
+            SqlDataReader dataReader = new SqlDataReader();                 // Retrieving your query's results
+
+            string connString = @"Server=INSTANCE_NAME;Database=DATABASE_NAME;Trusted_Connection = True;";         // Trusted Connection
+            //string connString = @"Server=INSTANCE_NAME;Database=DATABASE_NAME;User ID=USERNAME;Password=PASSWORD"; // SQL Authentication-based connection
+
+
+
+        }
+    }
+}
